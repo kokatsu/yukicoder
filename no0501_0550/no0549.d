dfdedb7b8cdb5a2aacd0d278e48ba5de8cc8715e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-10-31 15:13:19 +0000

    solve no0549

---
+++ no0501_0550/no0549.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto x = readln.chomp.split.to!(long[]);
+    
+    x.sort!"a > b";
+
+    long res = x[0];
+    foreach (i; 1 .. N) {
+        res += x[i] / 2;
+    }
+
+    res.writeln;
+}
