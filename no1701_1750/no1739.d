b1d204b1399a825e1689e07993bab362298f0235
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-11-12 13:07:02 +0000

    solve no1739

---
+++ no1701_1750/no1739.d
@@ -0,0 +1,58 @@
+import std;
+
+void main() {
+    long N, A, B, X, Y;
+    readf("%d %d %d %d %d\n", N, A, B, X, Y);
+
+    auto H = readln.chomp.split.to!(long[]);
+
+    long ok = 10 ^^ 9, ng = -1;
+    while (ok - ng > 1) {
+        long mid = (ok + ng) / 2;
+
+        auto D = H.dup;
+        D[] -= mid;
+        auto P = D.heapify;
+
+        bool isOK;
+        foreach (i; 0 .. A) {
+            if (P.empty) {
+                isOK = true;
+                break;
+            }
+
+            auto f = P.front;
+            P.popFront;
+
+            f = max(0, f-X);
+            if (f > 0) {
+                P.insert(f);
+            }
+        }
+
+        if (!isOK) {
+            long S;
+            while (!P.empty) {
+                auto f = P.front;
+                P.popFront;
+
+                if (f > 0) {
+                    S += f;
+                }
+            }
+
+            if (S <= B * Y) {
+                isOK = true;
+            }
+        }
+
+        if (isOK) {
+            ok = mid;
+        }
+        else {
+            ng = mid;
+        }
+    }
+    
+    ok.writeln;
+}
