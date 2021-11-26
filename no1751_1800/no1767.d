6222ea3683b7a5787e9f16e7851f22e29fe022ef
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-11-26 17:29:55 +0000

    solve no1767 with commentary

---
+++ no1751_1800/no1767.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(long[]);
+    auto B = readln.chomp.split.to!(long[]);
+
+    A = [long.min / 2] ~ A ~ [long.max / 2];
+
+    long res, pos;
+    foreach (i; 0 .. N+1) {
+        long[] C = [A[i]];
+        while (pos < M && B[pos] < A[i+1]) {
+            C ~= B[pos];
+            ++pos;
+        }
+
+        auto l = C.length;
+        if (l == 1) {
+            continue;
+        }
+
+        C ~= A[i+1];
+
+        long w;
+        foreach (j; 0 .. l) {
+            w = max(w, C[j+1]-C[j]);
+        }
+        
+        res += A[i+1] - A[i] - w;
+    }
+
+    res.writeln;
+}
