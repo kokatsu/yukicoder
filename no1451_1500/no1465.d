a7a57adfb73fb13a4f9da09d637c3d9096cc75c7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-07 11:01:46 +0000

    solve no1465

---
+++ no1451_1500/no1465.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto list = new bool[](N+1);
+
+    void f(int num, int cnt) {
+        if (cnt > K || list[num]) {
+            return;
+        }
+
+        list[num] = true;
+
+        if (num % 2 == 0) {
+            f(num/2, cnt+1);
+        }
+        
+        if (num > 3) {
+            f(num-3, cnt+1);
+        }
+    }
+
+    f(N, 0);
+
+    writeln(list[1] ? "YES" : "NO");
+}
