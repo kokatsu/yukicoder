6458948b5a2c95de89673682a81edb3a635d2df2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-14 14:08:06 +0000

    solve no0183

---
+++ no0151_0200/no0183.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int lim = A.maxElement * 2 + 1;
+    
+    auto list = new bool[](lim);
+    list[0] = true;
+    foreach (a; A) {
+        auto tmp = list.dup;
+        foreach (i; 0 .. lim) {
+            if (!list[i]) {
+                continue;
+            }
+
+            tmp[a^i] = true;
+        }
+        list = tmp;
+    }
+
+    list.count(true).writeln;
+}
