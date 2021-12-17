eb0e1c396d61d03fb38857feeeee45e6e9ce215d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-17 11:40:04 +0000

    solve no0419

---
+++ no0401_0450/no0419.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    real a, b;
+    readf("%f %f\n", a, b);
+
+    real res = hypot(a, b);
+    
+    if (a != b) {
+        if (a > b) {
+            swap(a, b);
+        }
+
+        res = min(res, sqrt(b*b-a*a));
+    }
+
+    format("%.10f", res).writeln;
+}
