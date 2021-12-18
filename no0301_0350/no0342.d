38feef516c85c156ab9a1e2243620ab74827e2a3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-18 11:53:06 +0000

    solve no0342

---
+++ no0301_0350/no0342.d
@@ -0,0 +1,52 @@
+import std;
+
+void main() {
+    dstring S = readln.chomp.to!dstring;
+
+    dchar[][] res;
+    dchar[] now;
+    dchar pre;
+    int cnt, num;
+
+    foreach (i, s; S) {
+        if (s == 'ｗ') {
+            ++num;
+        }
+        else {
+            if (pre == 'ｗ' && !now.empty) {
+                if (cnt < num) {
+                    cnt = num;
+                    res = [now];
+                }
+                else if (cnt == num) {
+                    res ~= now;
+                }
+                
+                now = [];
+                num = 0;
+            }
+
+            now ~= s;
+        }
+
+        pre = s;
+    }
+
+    if (pre == 'ｗ' && !now.empty) {
+        if (cnt < num) {
+            res = [now];
+        }
+        else if (cnt == num) {
+            res ~= now;
+        }
+    }
+
+    if (res.empty) {
+        writeln;
+    }
+    else {
+        foreach (r; res) {
+            r.writeln;
+        }
+    }
+}
