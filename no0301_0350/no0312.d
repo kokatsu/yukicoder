5872febe5a94c21cb1aca2cf4fcae661c60fab98
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-21 11:52:42 +0000

    solve no0312

---
+++ no0301_0350/no0312.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long S = N.to!real.sqrt.floor.to!long;
+    
+    long res = N;
+    foreach (i; 2 .. S+1) {
+        if (N % i == 0) {
+            if (i > 2) {
+                res = min(res, i);
+            }
+
+            if (N / i > 2) {
+                res = min(res, N/i);
+            }
+        }
+    }
+
+    res.writeln;
+}
