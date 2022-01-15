f148d31f1b140dc2d8f0004376c19b29ec1e18cc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-15 11:52:29 +0000

    solve no0668

---
+++ no0651_0700/no0668.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    string N = readln.chomp;
+
+    long M = N[0..3].to!long;
+    if (M % 10 >= 5) M += 10;
+
+    string S = M.to!string;
+    
+    long a = S[0] - '0';
+    long b = S[1] - '0';
+    long c = N.length + S.length - 4;
+
+    format("%d.%d*10^%d", a, b, c).writeln;
+}
