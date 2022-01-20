d5e861b5a6c11920e9911ac0219f2bca91ce8a79
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-20 10:36:07 +0000

    solve no0327

---
+++ no0301_0350/no0327.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    ++N;
+    dchar[] res;
+    while (N > 0) {
+        --N;   
+        res ~= to!dchar(N % 26 + 'A');
+        N /= 26;
+    }
+
+    res.reverse;
+
+    res.writeln;
+}
