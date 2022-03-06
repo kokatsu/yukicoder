98b477a76194106d7cbcd836d03debeec9865e47
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-06 11:39:08 +0000

    solve no0796

---
+++ no0751_0800/no0796.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto seq = new int[](N);
+    seq[0] = 3, seq[1..N] = 1;
+
+    if ((N + 2) % 3 == 0) ++seq[1];
+    else if ((N + 2) % 3 == 2) seq[1] += 2;
+
+     writefln("%(%s %)", seq);
+}
