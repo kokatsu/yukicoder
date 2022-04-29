6fa7d1de5886b356da14d0f501e138287f6a3d50
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-29 13:05:10 +0000

    solve no1915

---
+++ no1901_1950/no1915.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int T;
+    readf("%d\n", T);
+
+    foreach (_; 0 .. T) {
+        long N;
+        readf("%d\n", N);
+
+        long M = N * 2;
+        M.writeln;
+    }
+}
